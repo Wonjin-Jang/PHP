7c70412f86854c729c323f87c69121cc5b7b79de
Wonjin-Jang <bustlover11@gmail.com>
2021-03-01 16:40:18 +0900

    php syntax _ comparison

---
+++ basic/comparison.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>Comparison Operators &amp;
+Boolean data type </h1>
+
+<?php 
+var_dump (1==1);
+//var_dump = 출력 + 데이터 타입까지 출력
+//a=b 좌항은 우항이다
+//a==b 비교연산자 (boolean을 만들어냄) (같다면 true 다르다면 false)
+
+?>
+</body>
+</html>
