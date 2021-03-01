adcc762af2a3027bd41ea49617132f758e5ba8c9
Wonjin-Jang <bustlover11@gmail.com>
2021-03-01 17:15:13 +0900

    loop2

---
+++ basic/loop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Loop</title>
+</head>
+<body>
+<h1>While</h1>
+<?php 
+$i = 0;
+while ($i<10){
+echo '10번 반복하기<br>';
+$i= $i + 1 ;
+}
+?>
+</body>
+</html>
