08d623b4c567cd66f36c921848cacf09de63a2a7
Wonjin-Jang <bustlover11@gmail.com>
2021-03-01 17:05:50 +0900

    php loop1

--- basic/index.php
+++ basic/index.php
@@ -8,10 +8,28 @@
 <body>
     <h1><a href="index.php">WEB</a></h1>
         <ol>
+        <?php
+            $list = scandir('data');
+            $i = 0;
+            while($i<count($list)){
+                if($lsit[$i] !='.'){
+                    if($list[$i]) !='..'{
+        ?>
+        <li><a href="index.php?id=<?=$list[$i]?>"><?=$list[$i]?></a></li>
+        <?php
+                    }
+                }
+                $i = $i + 1; 
+            }
+            ?>
+    </ol>
+       <!-- 
+       <ol>
             <li><a href="index.php?id=HTML">HTML</a></li>
             <li><a href="index.php?id=CSS">CSS</a></li>
             <li><a href="index.php?id=JavaScript">JavaScript</a></li>
         </ol>
+        -->
     <h2>
         <?php
             if (isset($_GET['id'])){
