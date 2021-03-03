823d462fc40b5c54747ea5e20b914b3e9dee1c25
Wonjin-Jang <bustlover11@gmail.com>
2021-03-03 23:50:41 +0900

    PHP CR

--- create.php
+++ create.php
@@ -1,16 +1,31 @@
 <?php
-$conn = mysqli_connect(
-  'localhost',
-  'root',
-  '123123',
-  'opentutorials');
-$sql = "SELECT * FROM topic";
-$result = mysqli_query($conn, $sql);
-$list = '';
-while($row = mysqli_fetch_array($result)) { 
-  $list = $list."<li><a href=\"index.php?id={$row['id']}\"></a></li>";
-}
+    $conn = mysqli_connect(
+        "localhost", 
+        "root", 
+        "123123", 
+        "opentutorials"
+    );
+    $sql = "SELECT * FROM topic";
+    $result = mysqli_query($conn, $sql);
+    $list = '';
+    while($row = mysqli_fetch_array($result)){    
+        $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+    }
+
+    $article = array(
+        'title'=>'Welcome',
+        'description'=>'Hello !'
+    );
+
+    if(isset($_GET['id]'])){
+        $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_array($result);
+        $article['title'] = $row['title'];
+        $article['description'] = $row['description'];
+    }
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -18,9 +33,11 @@ while($row = mysqli_fetch_array($result)) {
         <title>WEB</title>
     </head>
     <body>
-        <h1>WEB</h1>
+        <h1><a href="index.php">WEB</a></h1>
         <ol>
-            <li>HTML</li>
+            <?=
+            $list
+            ?>
         </ol>
         <form action="process_create.php" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
--- index.php
+++ index.php
@@ -11,7 +11,21 @@
     while($row = mysqli_fetch_array($result)){    
         $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
     }
+
+    $article = array(
+        'title'=>'Welcome',
+        'description'=>'Hello !'
+    );
+
+    if(isset($_GET['id]'])){
+        $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_array($result);
+        $article['title'] = $row['title'];
+        $article['description'] = $row['description'];
+    }
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -19,14 +33,14 @@
         <title>WEB</title>
     </head>
     <body>
-        <h1>WEB</h1>
+        <h1><a href="index.php">WEB</a></h1>
         <ol>
             <?=
             $list
             ?>
         </ol>
         <a href="create.php">Create</a>
-        <h2>Welcome</h2>
-        Welcome 
+        <h2><?=$article['title']?></h2>
+        <?=$article['description']?>
     </body>
 </html>
